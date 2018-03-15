27e9212055042c3fa287a2b31b7c612a945f531e
PaindeMie <alexis.pister@hotmail.com>
2018-03-15 13:30:24 +0100

    debut fonction fitness

--- Individu.py
+++ Individu.py
@@ -2,12 +2,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
+import powerlaw
 
+#Importation du graphe de base et de ses caracteristiques
 Fb = nx.read_edgelist('facebook_combined.txt')
 Deg_dist_ref = nx.degree_histogram(Fb)
-N_ref = np.sum(Deg_dist_ref)
+Deg_prob_ref = np.array(Deg_dist_ref)/float(np.sum(np.array(Deg_dist_ref)))
+print list(Deg_prob_ref)
+PL = powerlaw.Fit(Deg_prob_ref)
+
+'''
+N_ref = np.sum(Deg_dist_ref) #Nb de noeuds
 CC_ref = nx.average_clustering(Fb)
 DI_ref = nx.diameter(Fb)
+'''
+N_ref = 4039 #Nb de noeuds
+CC_ref = 0.6055
+DI_ref = 8
+
+
+
 
 print N_ref
 
@@ -35,6 +49,7 @@ class Individu:
 		print "Distribution des degres :", self.DD
 		
 	def Maj_Fitness(self):
+		'''
 		#On ajuste la taille des deux liste en ajoutant des 0 dans la plus petite :
 		diff_taille = len(Deg_dist_ref)-len(self.DD)
 		if diff_taille > 0:
@@ -44,12 +59,21 @@ class Individu:
 			for i in range(diff_taille):
 				Deg_dist.append(0)
 		
-		#On calcule la différence de degrés par rapport au graphe de refférence
+		#On calcule la différence de degrés par rapport au graphe de reférence
 		Diff_DD = 0
 		for i in range(len(self.DD)):
 			Diff_DD = Diff_DD + (self.DD[i] - Deg_dist_ref[i])**2
 
 		print Diff_DD
+		'''
+		Diff_CC = (CC_ref - self.CC)**2
+		Diff_DI = (DI_ref - self.DI)**2
+		
+		
+		#Dist_prob = Deg_dist
+		
+		
+		
 
 
 I = Individu("SW", 20)
