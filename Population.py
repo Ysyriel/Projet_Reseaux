1458ad1fe35eae760e1dbea61addee8a36d27220
Paul <paul.denailly@laposte.net>
2018-03-19 17:23:03 +0100

    Fonction de sélection rendue fonctionnelle + Test de Run sur plusieurs itérations

--- Population.py
+++ Population.py
@@ -18,19 +18,18 @@ class Population:
 		for i in range(len(self.pop)):
 			print "Graphe %d:\n" %i, self.pop[i].matrix
 		
-	'''
+	
 	def selection(self):
 		list_W = [] #liste des fitness pour chaque individu
 		pioche = [] #liste de la "pioche", selon le fitness d'un individu son indice sera représenté plus ou moins de fois dans cette liste.
 		for i in range(self.nb):
-			list_W.append(self.pop[i].W) #Ajout des fitness pour chaque individu
-			print 
+			list_W.append(self.pop[i].W) #Ajout des fitness pour chaque individu 
 		W_total = np.sum(list_W)
-		for i in list_W :
-			nombre = (i/W_total)*100 #On crée le poids relatif de chaque fitness (par rapport à la somme totale des fitness).
-			np.concatenate([pioche,np.repeat(i,nombre)]) #Puis on incrémente la liste "pioche"
-		self.pop = self.pop[np.random.choice(pioche,self.nb,replace=True)] #Et on remet à jour la nouvelle pop de nb individus à partir des nb indices de "pioche".
-'''
+		for i,j in enumerate(list_W) :
+			nombre = (j/W_total)*100 #On crée le poids relatif de chaque fitness (par rapport à la somme totale des fitness).
+			pioche = np.concatenate([pioche,np.repeat(i,int(nombre))]) #Puis on incrémente la liste "pioche"
+		self.pop = [self.pop[int(i)] for i in np.random.choice(pioche,self.nb,replace=True)] #Et on remet à jour la nouvelle pop de nb individus à partir des nb indices de "pioche".
+
 		
 	def mutation(self):
 		indice = np.random.randint(1,1000) # Proba de 1/1000 qu'un individu mute
@@ -61,14 +60,19 @@ class Population:
 
 
 P = Population(2, "SF", 10)
-P.pmatrix()
-P.Crossing_over()
-P.pmatrix()
 
+#Run de test 
 
+print 'Test mises à jours de la population'
+for temps in range(5):
+	print "AU TEMPS",temps
+	for ind in P.pop :   #1) On calcule les fitness pour chaque individu
+		ind.Maj_Fitness()
+	P.selection()   #2) Selection des individus
+	P.Crossing_over() #3) Crossing overs sur les individus sélectionnés
+	P.mutation() #4) Mutation éventuelle
+	for ind in P.pop :   #1) Mise à jours des attributs
+		ind.Maj_attributs()
+		ind.prattribut()
+	print "=========================================================="
 
-'''
-print 'Test de malade'
-P.selection()
-P.pop[49].prattribut()
-'''
