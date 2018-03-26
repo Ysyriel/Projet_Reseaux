f940077ea5d4c613723e5c6429f6b867a1c49383
PaindeMie <alexis.pister@hotmail.com>
2018-03-26 21:51:24 +0200

    Merge

--- Population.py
+++ Population.py
@@ -88,13 +88,13 @@ for temps in range(10):
 	print "=========================================================="
 
 
-'''
+
 #Test crossing over
-for i in range(20):
-	print "AU TEMPS :", i
-	P.Pmatrix()
-	P.Crossing_over()
-'''
+for i in range(4):
+	print 'TEMPS :', i
+	P.Pfitness()
+	P.Maj_fitness()
+	P.Pfitness()
 
 
-print "TEMPS :", time.time() - t0
+#print "TEMPS :", time.time() - t0
